4729508d1c44a5db5185ace98a0b42e0b264de63
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-04-15 14:20:22 -0400

    Add config files and Ship class

---
+++ src/Ship.test.js
@@ -0,0 +1,13 @@
+const Ship = require("./Ship");
+
+describe("Ship", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship();
+  });
+});
+
+test("Check that the ship has a length", () => {
+  expect(ship);
+});

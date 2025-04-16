78a5ba6460ea390c1c6942d2e92f11a42e942b40
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-04-16 13:00:51 -0400

    Create tests for Ship.js

--- src/Ship.js
+++ src/Ship.js
@@ -1,5 +1,17 @@
 class Ship {
   constructor(length) {
     this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.hits += 1;
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
   }
 }
+
+module.exports = Ship;
--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,13 +1,31 @@
 const Ship = require("./Ship");
+console.log(`Ship: ${Ship}`);
 
 describe("Ship", () => {
   let ship;
 
   beforeEach(() => {
-    ship = new Ship();
+    ship = new Ship(4);
   });
-});
 
-test("Check that the ship has a length", () => {
-  expect(ship);
+  test("Check that the ship has a length", () => {
+    expect(ship.length).toBe(4);
+  });
+
+  test("Check that the ship is not sunk after creating", () => {
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  test("Check that the ship can be hit", () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  test("Check that the ship can be sunk", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
 });

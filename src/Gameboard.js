bd3f86183da75b6abc02c4fd475fb29aa2c4fb16
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-04-21 01:50:23 -0400

    Add  and

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,6 +1,29 @@
 class Gameboard {
   constructor(width, height) {
     this.size = [width, height];
+    this.board = this.createBoard(this.size);
+  }
+
+  placeShip(coordinates, orientation, length){
+  }
+
+  createBoard(size){
+    console.log("Creating board...");
+    return Array(size[1]).fill(Array(size[0]).fill("-"));
+  }
+
+  printBoard(){
+    let board = "";
+    for (let row of this.board){
+      board = board.concat(row.join(" "));
+      board = board.concat("\n");
+    }
+    console.log("BOARD:");
+    console.log(board);
+  }
+
+  receiveAttack(coordinates){
+    
   }
 }
 

d69ba06fa43d0549edd08a453d95ee16a64305d7
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-04-21 01:49:55 -0400

    Add tests to check board integrity

--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -6,4 +6,32 @@ describe("Gameboard tests", () => {
   beforeEach(() => {
     gameboard = new Gameboard(10, 10);
   });
+
+  test("Check that board exists after creation", () => {
+    const expectedBoard = [
+      ['-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-'],
+    ];
+    expect(gameboard.board).toEqual(expectedBoard);
+  })
+
+  test("Check that board has a size", () => {
+    expect(gameboard.size).toEqual([10,10]);
+  });
+
+  test("Check that ships can be placed", () => {
+    const expectedBoard = [
+      ['-', '-', '-', '-', 'x'],
+      ['-', '-', '-', '-', 'x'],
+      ['-', '-', '-', '-', 'x'],
+      ['-', '-', '-', '-', 'x'],
+      ['-', '-', '-', '-', 'x'],
+    ]
+    gameboard.placeShip([5, 5], "horizontal", 5);
+    expect(gameboard.board).toEqual(expectedBoard);
+  })
+  
 });

4e9e738d5c8f5daf25b079e5bc0897ee35afdbee
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-05-08 02:24:40 -0400

    Feat: Set up basic game UI and initialization
    Add initial HTML structure in `template.html`
    
    Add basic styling in `styles.css` to display boards side-by-side
    
    Implement initial JavaScript in `index.js` to:
    - Import necessary modules
    - Prompt for board size (placeholder)
    - Create two `Player` instances
    - Add a `displayBoard` function to dynamically display player's boards using tables
    - Call `displayBoard` to display both player's boards

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 // index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
+import Player from './Player';
 
-console.log(greeting);
+// let width = prompt("Enter board size:");
+let width = 10;
+while (!isInputValid(width)) {
+    alert('Invalid input, try again.');
+    width = prompt("Enter board size:");
+}
+
+function isInputValid(input) {
+    if (isNaN(input) || parseInt(input) < 5) {
+        return false;
+    }
+    return true;
+}
+
+const playerOne = new Player('real', width, width);
+const playerTwo = new Player('computer', width, width);
+const boardsContainer = document.querySelector('#boards-container');
+
+function displayBoard(player) {
+    const [width, height] = player.gameboard.size;
+    const boardTable = document.createElement('table');
+    for (let row = 0; row < height; row++) {
+        const tableRow = document.createElement('tr');
+        for (let column = 0; column < width; column++) {
+            const tableCell = document.createElement('td');
+            tableCell.setAttribute('data-x', `${column}`);
+            tableCell.setAttribute('data-y', `${row}`);
+            tableRow.appendChild(tableCell);
+        }
+        boardTable.appendChild(tableRow);
+    }
+    boardsContainer.appendChild(boardTable);
+}
+
+
+displayBoard(playerOne);
+displayBoard(playerTwo);

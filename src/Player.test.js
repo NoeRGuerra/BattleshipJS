abb4808425ef131810bfd54f04b4529e1d76fab8
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-05-08 00:20:56 -0400

    Add test cases for `Player.test.js`
    - Rewrote the description for all tests
    - Separated tests that were not granular enough into smaller tests
    - Added test case to check for non-string values for player type
    - Added test case to check for default gameboard size

--- src/Player.test.js
+++ src/Player.test.js
@@ -1,21 +1,42 @@
 const Player = require('./Player');
+const Gameboard = require('./Gameboard');
 
 describe('Player tests', () => {
-    test("Check that player type can be set as real or computer", () => {
-        const player1 = new Player('real');
-        const player2 = new Player('computer');
-
-        expect(player1.type).toBe('real');
-        expect(player2.type).toBe('computer');
+    test('should allow player type to be `real`', () => {
+        const player = new Player('real');
+        expect(player.type).toBe('real');
     });
 
-    test("Check that player type can't be set as an invalid value", () => {
+    test("should allow player type to be `computer`", () => {
+        const player = new Player('computer');
+        expect(player.type).toBe('computer');
+    });
+
+    test("should reject player types that are not `real` or `computer`", () => {
         expect(() => { new Player('shazam') }).toThrow(Error);
-        expect(() => { new Player(12) }).toThrow(Error);
+        expect(() => { new Player('abcdef') }).toThrow(Error);
+        expect(() => { new Player('human') }).toThrow(Error);
     });
 
-    test("Check board is created correctly", () => {
+    test('should reject player types that are not strings', () => {
+        expect(() => { new Player(undefined)}).toThrow(Error);
+        expect(() => { new Player(null)}).toThrow(Error);
+        expect(() => { new Player(12)}).toThrow(Error);
+        expect(() => { new Player({})}).toThrow(Error);
+    })
+
+    test("should create gameboard with correct size", () => {
         const player = new Player('real', 8, 8);
-        expect(player.gameboard.size).toEqual([8,8]);
+        expect(player.gameboard.size).toEqual([8, 8]);
+    });
+
+    test('should create gameboard with default size if no size is provided', () => {
+        const player = new Player('real');
+        expect(player.gameboard.size).toEqual([10, 10]);
+    });
+
+    test('should contain an instance of Gameboard', () => {
+        const player = new Player('real');
+        expect(player.gameboard).toBeInstanceOf(Gameboard);
     })
 })

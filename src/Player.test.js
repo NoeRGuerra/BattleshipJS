3237ba919c6df4a61248025f705bad5e57e3c491
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2025-06-01 13:17:27 -0400

    style: Apply Prettier formatting
    Automatically formatted files using Prettier to ensure consistent code style
    across the project. No functional changes were made in this commit.

--- src/Player.test.js
+++ src/Player.test.js
@@ -1,42 +1,56 @@
-const Player = require('./Player');
-const Gameboard = require('./Gameboard');
+const Player = require("./Player");
+const Gameboard = require("./Gameboard");
 
-describe('Player tests', () => {
-    test('should allow player type to be `real`', () => {
-        const player = new Player('real');
-        expect(player.type).toBe('real');
-    });
+describe("Player tests", () => {
+  test("should allow player type to be `real`", () => {
+    const player = new Player("real");
+    expect(player.type).toBe("real");
+  });
 
-    test("should allow player type to be `computer`", () => {
-        const player = new Player('computer');
-        expect(player.type).toBe('computer');
-    });
+  test("should allow player type to be `computer`", () => {
+    const player = new Player("computer");
+    expect(player.type).toBe("computer");
+  });
 
-    test("should reject player types that are not `real` or `computer`", () => {
-        expect(() => { new Player('shazam') }).toThrow(Error);
-        expect(() => { new Player('abcdef') }).toThrow(Error);
-        expect(() => { new Player('human') }).toThrow(Error);
-    });
+  test("should reject player types that are not `real` or `computer`", () => {
+    expect(() => {
+      new Player("shazam");
+    }).toThrow(Error);
+    expect(() => {
+      new Player("abcdef");
+    }).toThrow(Error);
+    expect(() => {
+      new Player("human");
+    }).toThrow(Error);
+  });
 
-    test('should reject player types that are not strings', () => {
-        expect(() => { new Player(undefined)}).toThrow(Error);
-        expect(() => { new Player(null)}).toThrow(Error);
-        expect(() => { new Player(12)}).toThrow(Error);
-        expect(() => { new Player({})}).toThrow(Error);
-    })
+  test("should reject player types that are not strings", () => {
+    expect(() => {
+      new Player(undefined);
+    }).toThrow(Error);
+    expect(() => {
+      new Player(null);
+    }).toThrow(Error);
+    expect(() => {
+      new Player(12);
+    }).toThrow(Error);
+    expect(() => {
+      new Player({});
+    }).toThrow(Error);
+  });
 
-    test("should create gameboard with correct size", () => {
-        const player = new Player('real', 8, 8);
-        expect(player.gameboard.size).toEqual([8, 8]);
-    });
+  test("should create gameboard with correct size", () => {
+    const player = new Player("real", 8, 8);
+    expect(player.gameboard.size).toEqual([8, 8]);
+  });
 
-    test('should create gameboard with default size if no size is provided', () => {
-        const player = new Player('real');
-        expect(player.gameboard.size).toEqual([10, 10]);
-    });
+  test("should create gameboard with default size if no size is provided", () => {
+    const player = new Player("real");
+    expect(player.gameboard.size).toEqual([10, 10]);
+  });
 
-    test('should contain an instance of Gameboard', () => {
-        const player = new Player('real');
-        expect(player.gameboard).toBeInstanceOf(Gameboard);
-    })
-})
+  test("should contain an instance of Gameboard", () => {
+    const player = new Player("real");
+    expect(player.gameboard).toBeInstanceOf(Gameboard);
+  });
+});
